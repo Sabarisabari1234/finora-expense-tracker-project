59cd289f3d5b26b7e256e98d714cfd4ef3dff8b2
SABARI M <140826711+Sabarisabari1234@users.noreply.github.com>
2026-09-09 18:47:22 +0530

    add

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -253,11 +253,17 @@ router.put("/change-password", authMiddleware, async (req, res) => {
 
     const user = await User.findById(req.userId);
 
-    if (user.isDemo) {
-      return res.status(403).json({
-        message: "Password cannot be changed for the demo account",
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
       });
     }
+
+    if (user.isDemo) {
+    return res.status(403).json({
+      message: "Password cannot be changed for the demo account",
+    });
+  }
 
     const isPasswordCorrect = await bcrypt.compare(
       currentPassword,

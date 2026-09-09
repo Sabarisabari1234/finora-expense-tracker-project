06af7608eb6242771f6655d31a970d13ee9b7831
SABARI M <140826711+Sabarisabari1234@users.noreply.github.com>
2026-09-09 10:13:15 +0530

    Prepare backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -25,8 +25,10 @@ mongoose
     .then(() => {
         console.log("MongoDb connected");
 
-        app.listen(5000, () => {
-        console.log("Server running on port 5000");
+        const PORT = process.env.PORT || 5000;
+
+        app.listen(PORT, "0.0.0.0", () => {
+        console.log(`Server running on port ${PORT}`);
         });
     })
     .catch((error) => {

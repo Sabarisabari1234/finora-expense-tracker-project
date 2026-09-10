6b86bcd77eb0c32335bb9fbf50f95a864393fda8
SABARI M <140826711+Sabarisabari1234@users.noreply.github.com>
2026-09-10 09:29:55 +0530

    ss

--- src/components/Dashboard/SpendingChart.jsx
+++ src/components/Dashboard/SpendingChart.jsx
@@ -49,7 +49,7 @@ function SpendingChart({ expenses, theme }) {
         <div className="h-72 w-full">
           <ResponsiveContainer width="100%" height={300}>
             <BarChart data={data}>
-              <CartesianGrid strokeDasharray="3 3" />
+              <CartesianGrid strokeDasharray="3 3" stroke="#475569" strokeOpacity={0.3}/>
 
               <XAxis
                 dataKey="name"
--- src/pages/Analytics.jsx
+++ src/pages/Analytics.jsx
@@ -104,7 +104,7 @@ function Analytics({ expenses, theme }) {
             <div className="h-[320px] w-full">
               <ResponsiveContainer width="100%" height="100%">
                 <LineChart data={monthlyData}>
-                  <CartesianGrid strokeDasharray="3 3"/>
+                  <CartesianGrid strokeDasharray="3 3" stroke="#475569" strokeOpacity={0.3}/>
 
                   <XAxis 
                     dataKey="month"
